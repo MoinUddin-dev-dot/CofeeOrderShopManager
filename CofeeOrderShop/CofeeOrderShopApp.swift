d7a28a6c1a4d3947600550505b0ceca67324e5f2
Moin UD-din <129534945+MoinUddin-dev-dot@users.noreply.github.com>
2025-04-24 10:45:44 +0500

    Initial Commit

---
+++ CofeeOrderShop/CofeeOrderShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CofeeOrderShopApp.swift
+//  CofeeOrderShop
+//
+//  Created by Moin on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct CofeeOrderShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CofeeOrderShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CofeeOrderShop
+//
+//  Created by Moin on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
